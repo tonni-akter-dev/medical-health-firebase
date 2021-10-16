cec3debc17d58ecb0ce35d7acdf80f60660c42dd
unknown <Tonni Akter>
2021-10-16 18:55:57 +0600

    sec

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBevD1Li7h_HUKYpW-J7u86Dst8E46FdCQ",
+    authDomain: "medical-b6fd0.firebaseapp.com",
+    projectId: "medical-b6fd0",
+    storageBucket: "medical-b6fd0.appspot.com",
+    messagingSenderId: "174968345332",
+    appId: "1:174968345332:web:9fa6b6dc966e95903184a1"
+  };
+  export default firebaseConfig;
